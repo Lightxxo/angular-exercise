4cda6f28efd213d76c59e5067f9c1bd5d4304ce8
Mahir Tasin Islam <light.g117@gmail.com>
2025-01-08 13:04:16 +0600

    completed home component

--- first-app_01-hello-world/src/app/app.component.ts
+++ first-app_01-hello-world/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import {Component} from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: `
-    <h1>Hello World!</h1>
-  `,
-  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent],
+  templateUrl : './app.component.html',
+  styleUrls: ['./app.component.css',],
 })
 export class AppComponent {
   title = 'homes';
---
+++ first-app_01-hello-world/src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  template: `
+    <section>
+      <form >
+        <input type="text" placeholder="Filter By City">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+
+    <section class="results">
+
+    </section>
+  `,
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+
+}
